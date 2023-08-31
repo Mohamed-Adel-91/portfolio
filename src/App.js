4465edd3583eb00e96103a439acf9a8fe80332c7
MohamedAdel <mohamed101291@gmail.com>
2023-08-31 07:48:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashLoader } from "react-spinners";
+import Home from "./Home";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+    }, []);
+    return (
+        <BrowserRouter>
+            {loading ? (
+                <div className="preLoader">
+                    <HashLoader
+                        color="#3d4abe"
+                        loading={loading}
+                        height={100}
+                        margin={10}
+                    />
+                </div>
+            ) : (
+                <>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </>
+            )}
+        </BrowserRouter>
+    );
+};
 
 export default App;
