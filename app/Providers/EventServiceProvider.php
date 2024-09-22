c59fd88d96a9e0703132e02f16feda371451248e
Mohamed-Adel-91 <mohamed101291@gmail.com>
2024-09-22 20:15:02 +0300

    try events

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,7 +5,8 @@ namespace App\Providers;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
+// use Illuminate\Support\Facades\Event;
+// use Illuminate\Auth\Listeners\EventListener;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,6 +19,12 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        // Event::class => [
+        //     EventListener,
+        // ],
+        'App\Events\Event'::class => [
+            'App\Listeners\EventListener',
+        ],
     ];
 
     /**
@@ -27,6 +34,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        parent::boot();
         //
     }
 
