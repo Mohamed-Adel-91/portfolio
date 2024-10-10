26cdf2fe0f9d8490a34d9cf7ef9d65854a78b537
Mohamed-Adel-91 <mohamed101291@gmail.com>
2024-10-10 20:39:53 +0300

    test java

---
+++ resources/views/test.java
@@ -0,0 +1,10 @@
+package com.codewithmosh;
+
+public class Main {
+    public void log (int[] numbers){
+        for (int i = 0; i < numbers.length; i++) 
+            System.out.printIn(numbers[i]);
+        for (int number : numbers)
+            System.out.printIn(number)
+    }
+}
