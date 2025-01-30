bb29ced041d774d8ff5af8e473a5de9a3cbc6923
Mohamed-Adel-91 <mohamed101291@gmail.com>
2025-01-30 19:19:05 +0200

    chore: update composer dependencies and adjust minimum stability settings; modify intro section to reflect role change

--- resources/views/web/intro.blade.php
+++ resources/views/web/intro.blade.php
@@ -5,14 +5,14 @@
                 <div class="col-md-8 col-sm-12 col-xs-12">
                     <div class="intro-caption">
                         <span>I am Mohamed Adel</span>
-                        <h2>Full-Stack Developer</h2>
+                        <h2>Backend Developer</h2>
                         <button class="button">Contact Me</button>
                     </div>
                 </div>
                 <div class="col-md-4 col-sm-12 col-xs-12">
                     <div class="intro-image">
                         <img src="images/profile.png" alt="Profile Image"
-                            style="clip-path:none; border-radius: 50%;" />
+                            style="clip-path:none; border-radius: 50%; opacity: 0;" />
                     </div>
                 </div>
             </div>
