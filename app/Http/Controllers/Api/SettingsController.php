861eb8fca704b05b8fb0fae867dda4831c0d9d03
Mohamed-Adel-91 <mohamed101291@gmail.com>
2025-05-06 20:41:05 +0300

    feat: implement ApiResponse helper for standardized JSON responses in SettingsController

--- app/Http/Controllers/Api/SettingsController.php
+++ app/Http/Controllers/Api/SettingsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\ApiResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\SettingResource;
 use App\Models\Setting;
@@ -18,12 +19,15 @@ class SettingsController extends Controller
     public function __invoke(Request $request)
     {
         // return first record or specific record
-        // $Setting = Setting::first();
-        // return new SettingResource($Setting);
-        // return response((new SettingResource($Setting))->toArray($request));
+        // $setting = setting::first();
+        // return new SettingResource($setting);
+        // return response((new SettingResource($setting))->toArray($request));
 
         // return all records
-        $Setting = Setting::all();
-        return SettingResource::collection($Setting);
+        // $setting = Setting::get();
+        // return SettingResource::collection($setting);
+        $setting = Setting::find(1);
+        if($setting) return ApiResponse::sendResponse(200, 'Settings retrieved successfully', new SettingResource($setting));
+        return ApiResponse::sendResponse(204, 'No Content, Settings Not Found', []);
     }
 }
