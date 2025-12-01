fe205b40f4dd0282bb75622a5a8bfb18b39117b2
Mohamed-Adel-91 <mohamed101291@gmail.com>
2025-12-01 21:52:59 +0200

    refactor style

--- resources/views/web/about.blade.php
+++ resources/views/web/about.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="box-content section-card">
             <div class="row">
-                <div class="col-md-6 col-sm-12 col-xs-12">
+                <div class="col-md-12 col-sm-12 col-xs-12">
                     <div class="content-left">
                         <div class="section-title section-title-left">
                             <h3>About Me</h3>
@@ -15,11 +15,11 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6 col-sm-12 col-xs-12">
+                {{-- <div class="col-md-6 col-sm-12 col-xs-12">
                     <div class="content-right"
                         style="background-image: url('{{ $about ? asset($about->image_path) : asset('2.jpg') }}');">
                     </div>
-                </div>
+                </div> --}}
             </div>
         </div>
     </div>
