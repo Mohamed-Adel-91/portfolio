fbda848c116106edc0ffdbc56df8e4c9c003d6af
Mohamed Adel <mohamed101291@gmail.com>
2025-12-02 14:32:31 +0200

    fix desc style

--- resources/views/web/about.blade.php
+++ resources/views/web/about.blade.php
@@ -2,24 +2,24 @@
     <div class="container">
         <div class="box-content section-card">
             <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="col-md-6 col-sm-12 col-xs-12">
                     <div class="content-left">
                         <div class="section-title section-title-left">
                             <h3>About Me</h3>
                         </div>
                         <div class="content">
                             <h2 class="section-heading">{{ $about ? $about->title : 'About Me' }}</h2>
-                            <p>
-                                {!! $about ? $about->description : '######'!!}
-                            </p>
+                            <div class="about-description">
+                                {!! $about ? $about->description : '######' !!}
+                            </div>
                         </div>
                     </div>
                 </div>
-                {{-- <div class="col-md-6 col-sm-12 col-xs-12">
+                <div class="col-md-6 col-sm-12 col-xs-12">
                     <div class="content-right"
                         style="background-image: url('{{ $about ? asset($about->image_path) : asset('2.jpg') }}');">
                     </div>
-                </div> --}}
+                </div>
             </div>
         </div>
     </div>
