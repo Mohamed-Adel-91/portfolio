b68e274e982cda246c86ed043d20dd6c7e556ed8
Mohamed-Adel-91 <mohamed101291@gmail.com>
2025-12-22 17:39:08 +0200

    add SEO

--- resources/views/web/layouts/master.blade.php
+++ resources/views/web/layouts/master.blade.php
@@ -3,14 +3,28 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="{{ asset('images/favicon.png') }}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0" />
+    <link rel="icon" href="{{ asset('assets/img/2.png') }}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('assets/img/2.png') }}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/2.png') }}" />
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/img/2.png') }}" />
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/img/2.png') }}" />
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/img/2.png') }}" />
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/img/2.png') }}" />
+    <link rel="canonical" href="https://www.mohamed-nouh.com/" />
+    <meta name="author" content="Mohamed Adel Nouh" />
+    <meta name="theme-color" content="#000000" />
+    <meta name="msapplication-TileColor" content="#000000" />
     <title>{{ optional($setting)->meta_title ?? config('app.name') }}</title>
-    <meta name="description" content="{{ optional($setting)->meta_description ?? '' }}">
     <meta name="keywords" content="{{ optional($setting)->meta_tags ?? '' }}">
-
-    @include('web.layouts.scripts.css')
+    <meta name="description" content="{{ optional($setting)->meta_description ?? '' }}">
+    <meta property="og:title" content="{{ optional($setting)->meta_title ?? config('app.name') }}" />
+    <meta property="og:description" content="{{ optional($setting)->meta_description ?? '' }}" />
+    <meta property="og:image" content="{{ $intro ? asset($intro->image_path) : asset('1.jpg') }}" />
+    <meta property="twitter:image" content="{{ $intro ? asset($intro->image_path) : asset('1.jpg') }}" />
     <!-- ========================= CSS ============================ -->
+    @include('web.layouts.scripts.css')
 
 </head>
 
